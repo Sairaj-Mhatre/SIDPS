6d7d697233259ec47f7515af6f77d0f3668684f3
Sairaj <90263537+Sairaj-Mhatre@users.noreply.github.com>
2024-08-17 17:39:36 +0530

    Delete set.py

--- set.py
+++
@@ -1,7 +0,0 @@
-with open('words.txt', 'r') as file:
-    words = file.read().splitlines()
-
-# Remove duplicates while preserving order
-unique_words = list(dict.fromkeys(words))
-
-print(unique_words)
